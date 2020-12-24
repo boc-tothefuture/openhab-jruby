c72ca26c3eeb1da292c88660b994a9c33a83d2b4
boc <boc@us.ibm.com>
2020-12-24 14:08:26 -0500

    refactor: Moved item_state_event into its own file and references from events.rb

--- lib/openhab/core/dsl/monkey_patch/events.rb
+++ lib/openhab/core/dsl/monkey_patch/events.rb
@@ -1,20 +1,3 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/ClassAndModuleChildren
-class Java::OrgOpenhabCoreItemsEvents::ItemStateChangedEvent
-  # rubocop:enable Style/ClassAndModuleChildren
-
-  def item
-    # rubocop:disable Style/GlobalVars
-    $ir.get(item_name)
-    # rubocop:enable Style/GlobalVars
-  end
-
-  def state
-    item_state
-  end
-
-  def last
-    old_item_state
-  end
-end
+require 'core/dsl/monkey_patch/events/item_state_changed'
