ae65705895731a0b3e2c819c399c70e33dccb54e
boc <boc@us.ibm.com>
2021-01-14 11:51:35 -0500

    build: replace colon with underscore in openhab cucumber log names

--- features/support/gather_logs.rb
+++ features/support/gather_logs.rb
@@ -2,6 +2,6 @@ require_relative 'openhab'
 
 After do |scenario|
   feature_log_file = File.join('tmp/cucumber_logs',
-                               "#{File.basename(scenario.location.file)}:#{scenario.location.lines.first}")
+                               "#{File.basename(scenario.location.file)}_#{scenario.location.lines.first}")
   FileUtils.cp openhab_log, feature_log_file
 end
