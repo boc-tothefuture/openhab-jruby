06f7850ab02755093f756971fe16a84f377ef15b
boc <boc@us.ibm.com>
2021-01-15 15:49:33 -0500

    Docs: Adding method/class documentation.

--- lib/openhab/core/dsl/items/items.rb
+++ lib/openhab/core/dsl/items/items.rb
@@ -9,6 +9,9 @@ module OpenHAB
       module Items
         # rubocop: disable Style/GlobalVars
         class Items < SimpleDelegator
+          # Fetches the named item from the the ItemRegistry
+          # @param [String] name
+          # @return Item from registry, nil if item missing or requested item is a Group Type
           def[](name)
             item = $ir.getItem(name)
             (item.is_a? GroupItem) ? nil : item
@@ -16,6 +19,8 @@ module OpenHAB
         end
 
         java_import org.openhab.core.items.GroupItem
+        # Fetches all non-group items from the item registry
+        # @return [OpenHAB::Core::DSL::Items::Items]
         def items
           Items.new(EntityLookup.decorate_items($ir.items.reject { |item| item.is_a? GroupItem }))
         end
