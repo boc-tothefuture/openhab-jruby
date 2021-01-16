31e43f7c6db8cafd82f1308346c7e0eb12b7174e
boc-tothefuture <broconne+github@gmail.com>
2021-01-16 12:58:45 -0500

    Build: Introduce CI/CD Builds (#69)

---
+++ features/support/gather_logs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'openhab'
+
+After do |scenario|
+  feature_log_file = File.join('tmp/cucumber_logs',
+                               "#{File.basename(scenario.location.file)}_#{scenario.location.lines.first}")
+  FileUtils.cp openhab_log, feature_log_file
+end
