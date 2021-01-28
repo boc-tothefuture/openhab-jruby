1775430233f3bf6d7f5f5f9ddd439f1a342b769d
boc <boc@us.ibm.com>
2021-01-28 16:19:06 -0500

    feat(gem-install): support installation as a gem
    Former-commit-id: d1a2cb61f9e857f2b1577222f249845f61b2b6fe

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
+# Execute these functions on start
+
 require_relative 'openhab'
+
+def openhab_deploy
+  system('rake openhab:deploy 1>/dev/null 2>/dev/null') || raise('Error Deploying Libraries')
+end
+
+def prepare_openhab
+  openhab_deploy
+  ensure_openhab_running
+end
+
+After('@reset_library') do
+  stop_openhab
+  clear_gem_path
+  openhab_deploy
+  system('rake openhab:services[force] 1>/dev/null 2>/dev/null') || raise('Error Updating Services')
+  start_openhab
+end
+
+prepare_openhab
