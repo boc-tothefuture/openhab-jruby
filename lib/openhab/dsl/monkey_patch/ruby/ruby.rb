1e7a195524b1c7321d6731e99cbd7d56386dd8b6
Anders Alfredsson <andersb86@gmail.com>
2021-02-06 16:56:16 +0100

    feat: add DateTime Item type
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>
    
    Former-commit-id: a3cc139d87b2df344bb1f0f78c3a68558e3e4fd5

--- lib/openhab/dsl/monkey_patch/ruby/ruby.rb
+++ lib/openhab/dsl/monkey_patch/ruby/ruby.rb
@@ -4,4 +4,5 @@
 require 'openhab/dsl/monkey_patch/ruby/range'
 require 'openhab/dsl/monkey_patch/ruby/number'
 require 'openhab/dsl/monkey_patch/ruby/string'
+require 'openhab/dsl/monkey_patch/ruby/time'
 require 'bigdecimal/util'
