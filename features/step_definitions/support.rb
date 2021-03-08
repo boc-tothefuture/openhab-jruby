d9d610970c981244d0d12aed1d18d48e0d58429f
Brian O'Connell <broconne@gmail.com>
2021-03-08 07:22:54 -0500

    feat(image): support for image items
    Initial image item support. Support for updating images from a URL, a
    Base 64 encoded image, a file and mime type pairing, and a byte array/mime type pairing.
    
    Support is added to extract the mime type and image bytes from the image
    item.
    
    fix(image): removed unused methods
    
    Co-authored-by: jimtng <2554958+jimtng@users.noreply.github.com>
    
    Former-commit-id: dacc7a8531dcbe256c16a7ac8685ecb0c7e5dcc1

--- features/step_definitions/support.rb
+++ features/step_definitions/support.rb
@@ -2,10 +2,17 @@
 
 # General non-openhab specific steps
 
+require 'fileutils'
+require 'tmpdir'
+
 Then('If I wait {int} seconds') do |int|
   sleep(int)
 end
 
-When('if I wait {int} seconds') do |int|
+When('(if )I wait {int} seconds') do |int|
   sleep(int)
+end
+
+Given('file {string} is in the system temp dir') do |filename|
+  FileUtils.cp(File.join(__dir__, '../assets', filename), File.join(Dir.tmpdir))
 end
