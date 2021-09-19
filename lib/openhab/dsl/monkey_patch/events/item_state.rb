cbe6e5c275a30f84ca2ee3e32f1e3ae773b48e7e
Cody Cutrer <cody@cutrer.us>
2021-09-19 13:25:46 -0600

    feat(events)!: wrap event.state same as item.state
    adds event.null?, event.undef?, and event.state? to the event object for
    both updated and changed triggers. add event.was_null?, event.was_undef?,
    and event.was? to the event object for changed triggers.
    
    BREAKING CHANGE:
     * event.state returns nil it's NULL or UNDEF.
     * event.last renamed to event.was (the predicate methods make much more
      sense calling it was instead of last, similar to Rail's tracking of
      changed attributes).
     * event.was returns nil if's NULL or UNDEF

--- lib/openhab/dsl/monkey_patch/events/item_state.rb
+++ lib/openhab/dsl/monkey_patch/events/item_state.rb
@@ -10,12 +10,51 @@ module OpenHAB
       #
       module Events
         java_import Java::OrgOpenhabCoreItemsEvents::ItemStateEvent
+        java_import Java::OrgOpenhabCoreTypes::UnDefType
+
+        # Helpers common to ItemStateEvent and ItemStateChangedEvent
+        module ItemStateUnDefTypeHelpers
+          #
+          # Check if the state == UNDEF
+          #
+          # @return [Boolean] True if the state is UNDEF, false otherwise
+          #
+          def undef?
+            item_state == UnDefType::UNDEF
+          end
+
+          #
+          # Check if the state == NULL
+          #
+          # @return [Boolean] True if the state is NULL, false otherwise
+          def null?
+            item_state == UnDefType::NULL
+          end
+
+          #
+          # Check if the state is defined (not UNDEF or NULL)
+          #
+          # @return [Boolean] True if state is not UNDEF or NULL
+          #
+          def state?
+            undef? == false && null? == false
+          end
+
+          #
+          # Get the item state
+          #
+          # @return [State] OpenHAB state if state is not UNDEF or NULL, nil otherwise
+          #
+          def state
+            item_state if state?
+          end
+        end
 
         #
         # MonkeyPatch with ruby style accessors
         #
         class ItemStateEvent
-          alias state item_state
+          include ItemStateUnDefTypeHelpers
         end
       end
     end
