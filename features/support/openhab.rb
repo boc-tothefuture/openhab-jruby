73214f897a8c89d1ab4235d7280aa3e1003d5040
boc-tothefuture <broconne+github@gmail.com>
2021-10-30 10:30:11 -0400

    Revert "build(ci): optimize CI builds by splitting features across number of …"

--- features/support/openhab.rb
+++ features/support/openhab.rb
@@ -132,28 +132,18 @@ def delete_things
   openhab_client('openhab:things clear')
 end
 
-# rubocop:disable Metrics/MethodLength
 def delete_rules
   FileUtils.rm Dir.glob(File.join(rules_dir, '*.rb'))
   deleted = false
-  retries = 0
-  begin
-    Rest.rules.each do |rule|
-      uid = rule['uid']
-      Rest.delete_rule(uid)
-      deleted = true
-    end
-  rescue StandardError
-    raise unless retries < 5
-
-    sleep 1
-    retry
+  Rest.rules.each do |rule|
+    uid = rule['uid']
+    Rest.delete_rule(uid)
+    deleted = true
   end
   return unless deleted
 
   wait_until(seconds: 30, msg: 'Rules not empty') { Rest.rules.length.zero? }
 end
-# rubocop:enable Metrics/MethodLength
 
 def delete_shared_libraries
   FileUtils.rm Dir.glob(File.join(ruby_lib_dir, '*.rb'))
