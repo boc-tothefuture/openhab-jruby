9d99ee62c7f57f8050ce57b996efcf1387e7e52a
Cody Cutrer <cody@cutrer.us>
2021-11-30 09:44:15 -0700

    feat(channel_trigger): allow setting channel triggers directly from thing/channel objects
    also, allow an array of things and cross it with the channels
    
    this is useful if you're auto-discovering the channels by filtering the known things

--- lib/openhab/dsl/rules/triggers/channel.rb
+++ lib/openhab/dsl/rules/triggers/channel.rb
@@ -15,16 +15,19 @@ module OpenHAB
         #
         # Creates a channel trigger
         #
-        # @param [Array] channels array to create triggers for on form of 'binding_id:type_id:thing_id#channel_id'
+        # @param [String, Channel, ChannelUID, Array<String, Channel, ChannelUID>] channels
+        #   channels to create triggers for in form of 'binding_id:type_id:thing_id#channel_id'
         #   or 'channel_id' if thing is provided
-        # @param [thing] thing to create trigger for if not specified with the channel
-        # @param [String] triggered specific triggering condition to match for trigger
+        # @param [String, Thing, ThingUID, Array<String, Thing, ThingUID>] thing
+        #   thing(s) to create trigger for if not specified with the channel
+        # @param [String, Array<String>] triggered specific triggering condition(s) to match for trigger
         #
-        #
-        def channel(*channels, thing: nil, triggered: nil, attach: nil)
-          channels.flatten.each do |channel|
-            channel = [thing, channel].join(':') if thing
-            logger.trace("Creating channel trigger for channel(#{channel}), thing(#{thing}), trigger(#{triggered})")
+        def channel(*channels, thing: nil, triggered: nil, attach: nil) # rubocop:disable Metrics/AbcSize
+          channels.flatten.product([thing].flatten).each do |(channel, t)|
+            channel = channel.uid if channel.is_a?(org.openhab.core.thing.Channel)
+            t = t.uid if t.is_a?(Thing)
+            channel = [t, channel].compact.join(':')
+            logger.trace("Creating channel trigger for channel(#{channel}), thing(#{t}), trigger(#{triggered})")
             [triggered].flatten.each do |trigger|
               create_channel_trigger(channel, trigger, attach)
             end
@@ -36,14 +39,13 @@ module OpenHAB
         #
         # Create a trigger for a channel
         #
-        # @param [Channel] channel to look for triggers
-        # @param [Trigger] trigger specific channel trigger to match
+        # @param [String] channel to look for triggers
+        # @param [String] trigger specific channel trigger to match
         #
         #
         def create_channel_trigger(channel, trigger, attach)
           config = { 'channelUID' => channel }
           config['event'] = trigger.to_s unless trigger.nil?
-          config['channelUID'] = channel
           logger.trace("Creating Change Trigger for #{config}")
           append_trigger(Trigger::CHANNEL_EVENT, config, attach: attach)
         end
