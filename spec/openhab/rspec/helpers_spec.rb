ceebfba96090f6144ceb8a716617e74df48285ce
Cody Cutrer <cody@cutrer.us>
2022-10-31 09:50:23 -0600

    fix load_rules spec

--- spec/openhab/rspec/helpers_spec.rb
+++ spec/openhab/rspec/helpers_spec.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
+# rubocop:disable RSpec/NamedSubject I need to use the implicit subject
 RSpec.describe OpenHAB::RSpec::Helpers do
-  subject(:helpers) { Object.new.tap { |o| o.extend described_class } }
-
   describe "load_rules" do
     it "respects start levels" do
       allow(Dir).to receive(:[]).and_return(%w[automation/filea.rb automation/sl30/filec.rb
                                                automation/filea.sl20.rb automation/sl30/fileb.rb])
       # rubocop:disable RSpec/SubjectStub
-      expect(helpers).to receive(:load).with("automation/filea.sl20.rb").ordered
-      expect(helpers).to receive(:load).with("automation/sl30/fileb.rb").ordered
-      expect(helpers).to receive(:load).with("automation/sl30/filec.rb").ordered
-      expect(helpers).to receive(:load).with("automation/filea.rb").ordered
+      expect(subject).to receive(:load).with("automation/filea.sl20.rb").ordered
+      expect(subject).to receive(:load).with("automation/sl30/fileb.rb").ordered
+      expect(subject).to receive(:load).with("automation/sl30/filec.rb").ordered
+      expect(subject).to receive(:load).with("automation/filea.rb").ordered
       # rubocop:enable RSpec/SubjectStub
-      load_rules
+      subject.load_rules
     end
   end
 end
+# rubocop:enable RSpec/NamedSubject
