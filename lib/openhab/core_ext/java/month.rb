99709a468a7661b6b6f4c7a80ce10e64a3c3d9bf
Cody Cutrer <cody@cutrer.us>
2022-11-08 10:11:28 -0700

    fix typo

--- lib/openhab/core_ext/java/month.rb
+++ lib/openhab/core_ext/java/month.rb
@@ -5,7 +5,7 @@ module OpenHAB
     module Java
       # Extend Month with helper method
       module Month
-        # Calcalute and memoize the maximum number of days in a year before this month
+        # Calculate and memoize the maximum number of days in a year before this month
         # @return [Number] maximum nummber of days in a year before this month
         def max_days_before
           @max_days_before ||= Month.values.select { |month| month < self }.sum(&:max_length)
