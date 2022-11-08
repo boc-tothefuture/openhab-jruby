488b3c85a66653c7b296f61bf8a43bf3fa7c0fd6
Cody Cutrer <cody@cutrer.us>
2022-11-08 10:32:27 -0700

    fix yardoc link translation with anchors

--- lib/openhab/yard/html_helper.rb
+++ lib/openhab/yard/html_helper.rb
@@ -9,7 +9,8 @@ module OpenHAB
 
         # re-link files in docs/*.md. They're written so they work on github without any
         # processing
-        result.gsub!(%r{<a href="(?:[A-Za-z0-9_/-]+/)*([A-Za-z0-9_-]+).md"}, "<a href=\"file.\\1.html\"")
+        result.gsub!(%r{<a href="(?:[A-Za-z0-9_/-]+/)*([A-Za-z0-9_-]+).md(#[A-Za-z0-9_/-]+)"},
+                     "<a href=\"file.\\1.html\\2\"")
         result
       end
     end
