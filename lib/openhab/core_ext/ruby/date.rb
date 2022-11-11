e440f5e564f8d53fdeea8ef371e12573f1cd4d7e
Cody Cutrer <cody@cutrer.us>
2022-11-10 17:54:27 -0700

    make Date and Time method overriding compatible with ActiveSupport

--- lib/openhab/core_ext/ruby/date.rb
+++ lib/openhab/core_ext/ruby/date.rb
@@ -1,90 +1,88 @@
 # frozen_string_literal: true
 
-module OpenHAB
-  module CoreExt
-    module Ruby
-      # Extensions to Date
-      module Date
-        ::Date.prepend(self)
+# Extensions to Date
+class Date
+  #
+  # Extends {#+} to allow adding a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  # @param [java.time.temporal.TemporalAmount] other
+  # @return [java.time.LocalDate] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  def plus_with_temporal(other)
+    return to_local_date + other if other.is_a?(java.time.temporal.TemporalAmount)
 
-        #
-        # Extends {#+} to allow adding a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        # @param [java.time.temporal.TemporalAmount] other
-        # @return [java.time.LocalDate] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        def +(other)
-          return to_local_date + other if other.is_a?(java.time.temporal.TemporalAmount)
+    plus_without_temporal(other)
+  end
+  alias_method :plus_without_temporal, :+
+  alias_method :+, :plus_with_temporal
 
-          super
-        end
-
-        #
-        # Extends {#-} to allow subtracting a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        # @param [java.time.temporal.TemporalAmount] other
-        # @return [java.time.LocalDate] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        def -(other)
-          case other
-          when java.time.temporal.TemporalAmount, java.time.LocalDate
-            to_local_date - other
-          else
-            super
-          end
-        end
-
-        # @return [java.time.LocalDate]
-        def to_local_date(_context = nil)
-          java.time.LocalDate.of(year, month, day)
-        end
-
-        # @return [java.time.Month]
-        def to_month
-          java.time.Month.of(month)
-        end
-
-        # @return [java.time.MonthDay]
-        def to_month_day
-          java.time.MonthDay.of(month, day)
-        end
-
-        # @param [java.time.ZonedDateTime, nil] context
-        #   A {ZonedDateTime ZonedDateTime} used to fill in missing fields
-        #   during conversion. {java.time.ZonedDateTime.now} is assumed if not given.
-        # @return [java.time.ZonedDateTime]
-        def to_zoned_date_time(context = nil)
-          to_local_date.to_zoned_date_time(context)
-        end
-
-        # @return [Integer, nil]
-        def <=>(other)
-          return super if other.is_a?(self.class)
-
-          return self <=> other.to_date(self) if other.is_a?(java.time.MonthDay)
-
-          if other.respond_to?(:coerce) && (lhs, rhs = coerce(self))
-            return lhs <=> rhs
-          end
-
-          super
-        end
-
-        #
-        # Convert `other` to Date, if possible.
-        #
-        # @param [#to_date] other
-        # @return [Array, nil]
-        #
-        def coerce(other)
-          return nil unless other.respond_to?(:to_date)
-          return [other.to_date(self), self] if other.method(:to_date).arity == 1
-
-          [other.to_date, self]
-        end
-
-        alias_method :inspect, :to_s
-      end
+  #
+  # Extends {#-} to allow subtracting a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  # @param [java.time.temporal.TemporalAmount] other
+  # @return [java.time.LocalDate] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  def minus_with_temporal(other)
+    case other
+    when java.time.temporal.TemporalAmount, java.time.LocalDate
+      to_local_date - other
+    else
+      minus_without_temporal(other)
     end
   end
+  alias_method :minus_without_temporal, :-
+  alias_method :-, :minus_with_temporal
+
+  # @return [java.time.LocalDate]
+  def to_local_date(_context = nil)
+    java.time.LocalDate.of(year, month, day)
+  end
+
+  # @return [java.time.Month]
+  def to_month
+    java.time.Month.of(month)
+  end
+
+  # @return [java.time.MonthDay]
+  def to_month_day
+    java.time.MonthDay.of(month, day)
+  end
+
+  # @param [java.time.ZonedDateTime, nil] context
+  #   A {ZonedDateTime ZonedDateTime} used to fill in missing fields
+  #   during conversion. {java.time.ZonedDateTime.now} is assumed if not given.
+  # @return [java.time.ZonedDateTime]
+  def to_zoned_date_time(context = nil)
+    to_local_date.to_zoned_date_time(context)
+  end
+
+  # @return [Integer, nil]
+  def compare_with_coercion(other)
+    return compare_without_coercion(other) if other.is_a?(self.class)
+
+    return self <=> other.to_date(self) if other.is_a?(java.time.MonthDay)
+
+    if other.respond_to?(:coerce) && (lhs, rhs = coerce(self))
+      return lhs <=> rhs
+    end
+
+    compare_without_coercion(other)
+  end
+  alias_method :compare_without_coercion, :<=>
+  alias_method :<=>, :compare_with_coercion
+
+  #
+  # Convert `other` to Date, if possible.
+  #
+  # @param [#to_date] other
+  # @return [Array, nil]
+  #
+  def coerce(other)
+    return nil unless other.respond_to?(:to_date)
+    return [other.to_date(self), self] if other.method(:to_date).arity == 1
+
+    [other.to_date, self]
+  end
+
+  alias_method :inspect, :to_s
 end
--- lib/openhab/core_ext/ruby/time.rb
+++ lib/openhab/core_ext/ruby/time.rb
@@ -1,77 +1,77 @@
 # frozen_string_literal: true
 
-module OpenHAB
-  module CoreExt
-    module Ruby
-      # Extensions to Time
-      module Time
-        ::Time.prepend(self)
+# Extensions to Time
+class Time
+  #
+  # @!method +(other)
+  #
+  # Extends {#+} to allow adding a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  # @param [java.time.temporal.TemporalAmount] other
+  # @return [java.time.ZonedDateTime] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
+  # @return [Time] If other is a Numeric
+  #
+  def plus_with_temporal(other)
+    return to_zoned_date_time + other if other.is_a?(java.time.temporal.TemporalAmount)
 
-        #
-        # Extends {#+} to allow adding a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        # @param [java.time.temporal.TemporalAmount] other
-        # @return [java.time.ZonedDateTime] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
-        # @return [Time] If other is a Numeric
-        #
-        def +(other)
-          return to_zoned_date_time + other if other.is_a?(java.time.temporal.TemporalAmount)
+    plus_without_temporal(other)
+  end
+  alias_method :plus_without_temporal, :+
+  alias_method :+, :plus_with_temporal
 
-          super
-        end
+  #
+  # @!method -(other)
+  #
+  # Extends {#-} to allow subtracting a {java.time.temporal.TemporalAmount TemporalAmount}
+  #
+  # @param [java.time.temporal.TemporalAmount] other
+  # @return [java.time.ZonedDateTime] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
+  # @return [Time] If other is a Numeric
+  #
+  def minus_with_temporal(other)
+    return to_zoned_date_time - other if other.is_a?(java.time.temporal.TemporalAmount)
 
-        #
-        # Extends {#-} to allow subtracting a {java.time.temporal.TemporalAmount TemporalAmount}
-        #
-        # @param [java.time.temporal.TemporalAmount] other
-        # @return [java.time.ZonedDateTime] If other is a {java.time.temporal.TemporalAmount TemporalAmount}
-        # @return [Time] If other is a Numeric
-        #
-        def -(other)
-          return to_zoned_date_time - other if other.is_a?(java.time.temporal.TemporalAmount)
+    minus_without_temporal(other)
+  end
+  alias_method :minus_without_temporal, :-
+  alias_method :-, :minus_with_temporal
 
-          super
-        end
+  # @return [java.time.LocalDate]
+  def to_local_date(_context = nil)
+    java.time.LocalDate.of(year, month, day)
+  end
 
-        # @return [java.time.LocalDate]
-        def to_local_date(_context = nil)
-          java.time.LocalDate.of(year, month, day)
-        end
+  # @return [java.time.LocalTime]
+  def to_local_time
+    java.time.LocalTime.of(hour, min, sec, nsec)
+  end
 
-        # @return [java.time.LocalTime]
-        def to_local_time
-          java.time.LocalTime.of(hour, min, sec, nsec)
-        end
+  # @return [java.time.Month]
+  def to_month
+    java.time.Month.of(month)
+  end
 
-        # @return [java.time.Month]
-        def to_month
-          java.time.Month.of(month)
-        end
+  # @return [java.time.MonthDay]
+  def to_month_day
+    java.time.MonthDay.of(month, day)
+  end
 
-        # @return [java.time.MonthDay]
-        def to_month_day
-          java.time.MonthDay.of(month, day)
-        end
+  # @param [java.time.ZonedDateTime, nil] context
+  #   A {java.time.ZonedDateTime ZonedDateTime} used to fill in missing fields
+  #   during conversion. Not used in this class.
+  # @return [java.time.ZonedDateTime]
+  def to_zoned_date_time(_context = nil)
+    to_java(ZonedDateTime)
+  end
 
-        # @param [java.time.ZonedDateTime, nil] context
-        #   A {java.time.ZonedDateTime ZonedDateTime} used to fill in missing fields
-        #   during conversion. Not used in this class.
-        # @return [java.time.ZonedDateTime]
-        def to_zoned_date_time(_context = nil)
-          to_java(ZonedDateTime)
-        end
-
-        #
-        # Converts to a {java.time.ZonedDateTime ZonedDateTime} if `other`
-        # is also convertible to a ZonedDateTime.
-        #
-        # @param [#to_zoned_date_time] other
-        # @return [Array, nil]
-        #
-        def coerce(other)
-          [other.to_zoned_date_time(to_zoned_date_time), self] if other.respond_to?(:to_zoned_date_time)
-        end
-      end
-    end
+  #
+  # Converts to a {java.time.ZonedDateTime ZonedDateTime} if `other`
+  # is also convertible to a ZonedDateTime.
+  #
+  # @param [#to_zoned_date_time] other
+  # @return [Array, nil]
+  #
+  def coerce(other)
+    [other.to_zoned_date_time(to_zoned_date_time), self] if other.respond_to?(:to_zoned_date_time)
   end
 end
