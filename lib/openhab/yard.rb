3ec62b7f2cd84dc334e1bbe58c6fccd119bc3818
Cody Cutrer <cody@cutrer.us>
2022-11-14 08:02:45 -0700

    ensure YARD knows about core ruby objecs we mixin from/to

--- lib/openhab/yard.rb
+++ lib/openhab/yard.rb
@@ -6,3 +6,24 @@ end
 
 YARD::Templates::Template.extra_includes << ->(opts) { OpenHAB::YARD::HtmlHelper if opts.format == :html }
 YARD::Templates::Engine.register_template_path File.expand_path("../../templates", __dir__)
+
+#
+# @!parse
+#   # @!visibility private
+#   module Comparable; end
+#
+#   # @!visibility private
+#   module Forwardable; end
+#
+#   # @!visibility private
+#   module Singleton; end
+#
+#   # Extensions to Ruby Numeric
+#   class Numeric; end
+#
+#   # @!visibility private
+#   class Object; end
+#
+#   # Extensions to Ruby Range
+#   class Range; end
+#
