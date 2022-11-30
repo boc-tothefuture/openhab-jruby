3ba1054adff6028d161da3e10349e5a8236b48e9
Cody Cutrer <cody@cutrer.us>
2022-11-30 13:39:22 -0700

    implement dependency tracking
    inject ourselves into file and gem loading, and then notify
    OpenHAB of every file and gem that's loaded
    
    depends on https://github.com/openhab/openhab-addons/pull/13810
    to have any effect

---
+++ lib/openhab/core/dependency_tracking.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/GlobalVars
+if $dependency_listener
+  require "jruby"
+
+  klass = Class.new(org.jruby.util.collections.StringArraySet) do
+    def initialize(other)
+      super(JRuby.runtime)
+      other.each { |feature| append(feature) }
+    end
+
+    def append(name)
+      $dependency_listener.accept(name)
+
+      super
+    end
+  end
+
+  JRuby.runtime.load_service.class.field_accessor :loadedFeatures
+  JRuby.runtime.load_service.loadedFeatures = klass.new(JRuby.runtime.load_service.loadedFeatures)
+
+  loaded_specs = Gem.loaded_specs
+  loaded_specs.each_key do |gem|
+    $dependency_listener.accept("gem:#{gem}")
+  end
+
+  def loaded_specs.[]=(gem, _spec)
+    super
+
+    $dependency_listener.accept("gem:#{gem}")
+  end
+else
+  logger.warn("Dependency listener not found; dependency tracking disabled.")
+end
+# rubocop:enable Style/GlobalVars
