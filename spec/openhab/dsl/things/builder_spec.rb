46e4d389163d66d29c4bab944af320783224561c
Jimmy Tanagra <jcode@tanagra.id.au>
2022-12-01 14:03:23 +1000

    add spec for channel and thing builder

--- spec/openhab/dsl/things/builder_spec.rb
+++ spec/openhab/dsl/things/builder_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe OpenHAB::DSL::Things::Builder do
     end
     expect(things["astro:sun:home"]).not_to be_nil
   end
+
+  it "can use symbols for config keys" do
+    things.build do
+      thing "astro:sun:home", "Astro Sun Data", config: { geolocation: "0,0" }
+    end
+    expect(home = things["astro:sun:home"]).not_to be_nil
+    expect(home.configuration.get("geolocation")).to eq "0,0"
+  end
+
+  it "can create channels" do
+    things.build do
+      thing "astro:sun:home" do
+        channel "channeltest", "string", config1: "testconfig"
+      end
+    end
+    expect(home = things["astro:sun:home"]).not_to be_nil
+    expect(home.channels.map(&:uid).map(&:to_s)).to include("astro:sun:home:channeltest")
+    channel = home.channels.find { |c| c.uid.id == "channeltest" }
+    expect(channel.configuration.properties).to have_key("config1")
+    expect(channel.configuration.get("config1")).to eq "testconfig"
+  end
 end
