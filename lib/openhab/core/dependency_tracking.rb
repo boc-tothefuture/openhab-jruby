77c0bddd49fcc9b139420b10d838f154f87a0ac2
Cody Cutrer <cody@cutrer.us>
2022-12-04 19:40:27 -0700

    $dependency_listener was renamed in openhab-addons

--- lib/openhab/core/dependency_tracking.rb
+++ lib/openhab/core/dependency_tracking.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if defined?($dependency_listener)
+if defined?($dependencyListener)
   require "jruby"
 
   klass = Class.new(org.jruby.util.collections.StringArraySet) do
@@ -10,7 +10,7 @@ if defined?($dependency_listener)
     end
 
     def append(name)
-      $dependency_listener.accept(name)
+      $dependencyListener.accept(name)
 
       super
     end
@@ -21,13 +21,13 @@ if defined?($dependency_listener)
 
   loaded_specs = Gem.loaded_specs
   loaded_specs.each_key do |gem|
-    $dependency_listener.accept("gem:#{gem}")
+    $dependencyListener.accept("gem:#{gem}")
   end
 
   def loaded_specs.[]=(gem, _spec)
     super
 
-    $dependency_listener.accept("gem:#{gem}")
+    $dependencyListener.accept("gem:#{gem}")
   end
 else
   logger.warn("Dependency listener not found; dependency tracking disabled.")
