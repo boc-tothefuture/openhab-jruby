9262f8c691e6d09a1af7dc0ee402e4048b8227eb
Cody Cutrer <cody@cutrer.us>
2022-12-08 09:17:56 -0700

    fix files not showing up on all pages of docs

--- templates/default/fulldoc/html/setup.rb
+++ templates/default/fulldoc/html/setup.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+def init
+  options.objects = objects = run_verifier(options.objects)
+
+  generate_assets
+  options.files.each_with_index do |file, _i|
+    serialize_file(file, file.title)
+  end
+
+  options.delete(:objects)
+
+  objects.each do |object|
+    serialize(object)
+  rescue => e
+    path = options.serializer.serialized_path(object)
+    log.error "Exception occurred while generating '#{path}'"
+    log.backtrace(e)
+  end
+end
+
 def serialize(object)
   return if object == "_index.html"
 
